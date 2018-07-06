fcd8fcae22a6e10a05825cae090a28db056341ab
DiDex <wc34fun@mail.ru>
2018-07-06 18:47:02 +0800

    add greeting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BG from "./img/background.jpg";
+import styled from "styled-components";
+import Grid from "react-bootstrap/lib/Grid";
+import Row from "react-bootstrap/lib/Row";
+import Col from "react-bootstrap/lib/Col";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+        <h1>Hello {'world'}</h1>
+        <img src={BG} alt=""/>
+      </React.Fragment>
     );
   }
 }
